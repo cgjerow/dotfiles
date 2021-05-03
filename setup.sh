8a2dbabef85315aac24d23bdf6e39ef235f71906
connorjerow <cgjerow@gmail.com>
2021-05-03 12:10:20 -0700

    only clone if vundle directory does not exist

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+[ -d 	"~/.vim/bundle/Vundle.vim" ] && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim # clone if directory doesn't exist
 vim +PluginInstall +qall
 
 ln -sf ~/dotfiles/.zshrc ~/.zshrc && source ~/.zshrc
-ln -sf ~/dotfiles/.vimrc ~/.vimrc && source ~/.vimrc
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
