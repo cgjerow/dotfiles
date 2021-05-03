ad16eb8a2d33ba676d7f99be04a6459a40b4fd98
connorjerow <cgjerow@gmail.com>
2021-05-03 12:23:13 -0700

    clean plugins during setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 [ -d 	"~/.vim/bundle/Vundle.vim" ] && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim # clone if directory doesn't exist
-vim +PluginInstall +qall
+vim +PluginInstall +PluginClean +qall
 
 ln -sf ~/dotfiles/.zshrc ~/.zshrc && source ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
