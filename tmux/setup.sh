1bed9932c0e93b7ee862b7873a5ac3ba30bd789f
connorjerow <cgjerow@gmail.com>
2021-05-25 11:25:31 -0700

    use current window size for new session setup

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -10,10 +10,11 @@ tmuxSession ()
 	if [ $? != 0 ]
 	then
 		cd
-		tmux new-session -d -s $session -n main 
+        set -- $(stty size) # $1 = rows $2 = columns
+		tmux new-session -d -x "$2" -y "$(($1 - 1))" -s $session -n main 
 		tmux selectw -t $session:main
-		tmux splitw -v -p 10
-		tmux selectp -t 0
+		tmux splitw -v -p 30
+		tmux selectp -t 1
 		tmux splitw -h
 	
 		tmux new-window -n stats
