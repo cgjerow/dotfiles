e4bb3d84ab2a5b456fd621176b83846982112ec0
Connor Jerow <connor@extend.com>
2021-06-25 11:18:07 -0700

    change color of git branch in prompt

--- shell/prompt.sh
+++ shell/prompt.sh
@@ -5,4 +5,4 @@ precmd() { vcs_info }
 zstyle ':vcs_info:git:*' formats '[%b]'
 
 setopt prompt_subst
-export PROMPT='me:%F{51}%1~%f${vcs_info_msg_0_} '
+export PROMPT='me:%F{51}%1~%f%F{207}${vcs_info_msg_0_}%f '
