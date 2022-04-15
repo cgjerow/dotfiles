7f5577456c8b3eaf77b9d0504e931cc4520f0995
Connor Jerow <connor@extend.com>
2022-04-15 10:41:25 -0700

    dotfiles cd shortcut

--- shell/scripts.sh
+++ shell/scripts.sh
@@ -6,11 +6,13 @@ alias gc='git commit -m'
 alias gs='git status'
 alias gl='git log --all --graph --decorate --oneline'
 alias gp='git push -u'
+alias gcm='git checkout master'
 git config --global push.default current
 
 # Basic commands
 alias rsrc='source ~/.zshrc'
 alias dotfiles='cd ~/dotfiles'
+alias dot='dotfiles'
 alias e="$EDITOR"
 alias vi='e'
 alias vim='e'
